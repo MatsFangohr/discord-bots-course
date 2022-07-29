7e59a799cd345f908778024fc0ad7cdfc7037cf6
Mats Fangohr <matsfangohr@gmail.com>
2022-07-29 12:59:09 +0200

    add modules with explaination and example

---
+++ notebooks/testmodul.py
@@ -0,0 +1,17 @@
+pi = 3.14159265358979323846
+
+
+def fakultaet(zahl):
+    ergebnis = 1
+    for i in range(1, zahl + 1):
+        ergebnis = ergebnis * i
+    return ergebnis
+
+
+def magie(zahl):
+    return fakultaet(int(8 * zahl / 2 % 9))
+
+
+print([magie(x) for x in range(50)])
+
+print("Diese Nachricht kannst du erstmal ignorieren.")
