4b70a1c73de6b170ff191e318aaf79c34f750e36
Mats Fangohr <matsfangohr@gmail.com>
2022-08-16 17:19:46 +0200

    add bleib_an.py

---
+++ kurs/bleib_an.py
@@ -0,0 +1,21 @@
+def start():
+    print("bot bleibt an!")
+
+
+# auf replit ist es möglich, den bot mit einer solchen datei am leben zu halten. mehr details dazu in discordpy_einfühung.py
+
+# from flask import Flask
+# from threading import Thread
+
+# app = Flask("")
+
+# @app.route("/")
+# def home():
+#     return "I'm alive"
+
+# def run():
+#   app.run(host='0.0.0.0',port=8080)
+
+# def start():
+#     t = Thread(target=run)
+#     t.start()
